859f84cfe512422dfef5062135b5f3384582c5e7
TCodes <theodoreuokeke@gmail.com>
2023-08-14 15:39:16 +0100

    fixed quantity and price increment in the cart

--- public/main.js
+++ public/main.js
@@ -25,10 +25,44 @@ function ready(){
         var button = removeCartButtons[i]
         button.addEventListener('click', removeCartItem)
     }
-}
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for(var i = 0; i < quantityInputs.length; i++){
+        for (var j = 0; j < quantityInputs.length; j++){
+            var input = quantityInputs[i]
+            input.addEventListener('change', quantityChanged)
+    }
+}}
 
 //functionality to remove items from the cart
 function removeCartItem(event){
     var buttonClicked = event.target
     buttonClicked.parentElement.remove()
+    updatetotal();
+}
+//functionality to change quantity of items in the cart
+function quantityChanged(event){
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    }
+    updatetotal();
+}
+
+
+
+//functionality to total the cart
+function updatetotal(){
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    var cartBoxes = cartContent.getElementsByClassName('cart-box')
+    var total = 0
+    for(var i = 0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i]
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('total-price')[0].innerText = '$' + total
 }
