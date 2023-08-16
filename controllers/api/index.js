21053d9179013ae1f1e9a773de0d28be109dd7c4
unknown <lukeharriman1013@gmail.com>
2023-08-16 03:08:23 -0700

    starter functions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const cartRoutes = require('./cartRoutes');
 const catRoutes = require('./catRoutes');
 const userRoutes = require('./userRoutes');
+const walletRoute = require('./walletRoute');
 
 router.use("/users", userRoutes);
 router.use("/cart", cartRoutes);
 router.use("/cats", catRoutes);
+router.use("/wallet", walletRoute);
 
 module.exports = router;
