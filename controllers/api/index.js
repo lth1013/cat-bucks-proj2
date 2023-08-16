1b127ca009cc4b5151cce8e97854222658b0c01f
TheLastVelo <jarellchinn44@gmail.com>
2023-08-16 09:57:52 -0700

    added heroku

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const cartRoutes = require('./cartRoutes');
-const catRoutes = require('./catRoutes');
-const userRoutes = require('./userRoutes');
-const walletRoute = require('./walletRoute');
+const router = require("express").Router();
+const cartRoutes = require("./cartRoutes");
+const catRoutes = require("./catRoutes");
+const userRoutes = require("./userRoutes");
+const walletRoute = require("./walletRoute");
 
+router.use("/wallet", walletRoute);
 router.use("/users", userRoutes);
 router.use("/cart", cartRoutes);
 router.use("/cats", catRoutes);
-router.use("/wallet", walletRoute);
 
 module.exports = router;
